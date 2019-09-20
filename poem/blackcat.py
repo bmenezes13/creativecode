d9fe7f090e3e89f0cd6cf44dacc3ca4b827b8284
bmenezes13 <31707708+bmenezes13@users.noreply.github.com>
2019-09-20 13:10:21 -0400

    Update blackcat.py

--- poem/blackcat.py
+++ poem/blackcat.py
@@ -25,16 +25,15 @@ for word,pos in blob.tags:
         prep.append(word)
     if (pos == 'DT'):
         determ.append(word)
-#Takes a determiner, two nouns, two adjectives, two prepositions and a verb
+#Takes a determiner, a noun, two adjectives, two prepositions and a verb
 #to form four couplets for each poem
 for i in range(4):
+    dt = random.choice(determ)
     n1 = random.choice(noun)
-    n2 = random.choice(noun)
     a1 = random.choice(adject)
     a2 = random.choice(adject)
-    vb = random.choice(verb)
     p1 = random.choice(prep)
     p2 = random.choice(prep)
-    dt = random.choice(determ)
+    vb = random.choice(verb)
     print(dt + " " + n1 + " " + p1 + " " + a2 +"\n" +
-          vb  + " " + p2 + " " + a1 + " " + n2 + "\n")
+          vb  + " " + p2 + " " + a1 + "\n")
